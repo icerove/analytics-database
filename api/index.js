ee7bcec851a6ec0093505bfcdeacf214fbb494c6
Yifang Ma <yifang@nearprotocol.com>
2021-07-05 09:53:42 +0800

    fix typo

--- api/index.js
+++ api/index.js
@@ -1,31 +1,31 @@
-const { Router } = require('express')
-const {pool, sql} = require('../db')
+const { Router } = require('express');
+const { pool, sql } = require('../db');
 
-const auth = require('./auth')
-const project = require('./project')
-const query = require('./query')
-const result = require('./result')
+const auth = require('./auth');
+const project = require('./project');
+const query = require('./query');
+const result = require('./result');
 
-const router = new Router()
+const router = new Router();
 
-router.use('/user', auth)
-router.use('/project', project)
-router.use('/query', query)
-router.use('./result', result)
+router.use('/user', auth);
+router.use('/project', project);
+router.use('/query', query);
+router.use('./result', result);
 
 const getProjects = async (req, res) => {
-  result = await pool.query(sql.getAllProjects())
-  res.json(result.rows)
-}
+  project_result = await pool.query(sql.getAllProjects());
+  res.json(project_result.rows);
+};
 
 const findProject = async (req, res) => {
-  projectName = req.body.projectName
+  projectName = req.body.projectName;
 
-  result = await pool.query(sql.findProject({projectName}))
-  res.json(result.rows[0])
-}
+  search_result = await pool.query(sql.findProject({ projectName }));
+  res.json(search_result.rows[0]);
+};
 
-router.get('/', getProjects)
-router.get('/search', findProject)
+router.get('/', getProjects);
+router.get('/search', findProject);
 
-module.exports = router
+module.exports = router;
