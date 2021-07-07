8a662f0bd91de75ce8aea4c23a4d1e86daa1d85c
Yifang Ma <yifang@nearprotocol.com>
2021-07-07 16:58:50 +0800

    change column

--- migrations/20210621074055-add-result.js
+++ migrations/20210621074055-add-result.js
@@ -5,36 +5,39 @@ var type;
 var seed;
 
 /**
-  * We receive the dbmigrate dependency from dbmigrate initially.
-  * This enables us to not have to rely on NODE_PATH.
-  */
-exports.setup = function(options, seedLink) {
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
   dbm = options.dbmigrate;
   type = dbm.dataType;
   seed = seedLink;
 };
 
-exports.up = function(db) {
+exports.up = function (db) {
   return db.createTable('results', {
     result_id: { type: 'SERIAL', primaryKey: true },
-    result: 'text',
-    executed_at: {type: 'TIMESTAMP'},
-    query_id:  {type: 'int', foreignKey: {
-      name: 'query_result_id',
-      table: 'queries',
-      rules: {
-        onDelete: 'CASCADE',
-        onUpdate: 'RESTRICT'
+    query_result: 'text',
+    executed_at: { type: 'TIMESTAMP' },
+    query_id: {
+      type: 'int',
+      foreignKey: {
+        name: 'query_result_id',
+        table: 'queries',
+        rules: {
+          onDelete: 'CASCADE',
+          onUpdate: 'RESTRICT',
+        },
+        mapping: 'query_id',
       },
-      mapping: 'query_id'}
-    }
-  })
+    },
+  });
 };
 
-exports.down = function(db) {
-  return db.dropTable('results')
+exports.down = function (db) {
+  return db.dropTable('results');
 };
 
 exports._meta = {
-  "version": 1
+  version: 1,
 };
